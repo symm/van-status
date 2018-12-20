053f017d8b1f4ce38d72096e579a8b5d88199f55
Derek Kaye <github@dezzanet.co.uk>
2018-12-20 14:21:02 +0000

    tweak colours etc

--- run.js
+++ run.js
@@ -13,13 +13,39 @@ function setColour(red, green, blue, white) {
   neopixel.write(B15, data);
 }
 
-function onInit(){
+function red() {
   setColour(255, 0, 0, 0);
+}
 
+function amber() {
+  setColour(200, 200, 0, 0);
+}
 
+function blue() {
+  setColour(0, 50, 200, 0);
+}
+
+function green() {
+  setColour(0, 255, 0, 0);
+}
+
+function off() {
+  setColour(0, 0, 0, 0);
+}
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+var wifiInitialised = false;
+var busInitialised = false;
+
+function onInit(){
+  red();
   wifi.connect("O2 Wifi", {}, function(err){
+    wifiInitialised = true;
     console.log("connected:", err);
-     setColour(0, 255, 0, 0);
+     amber();
 
     wifi.getIP(function(err, ipinfo){
       console.log(ipinfo);
@@ -29,12 +55,28 @@ function onInit(){
       host: "10.204.82.73",
     });
     mqtt.subscribe("test/espruino");
+    blue();
 
     mqtt.on('publish', function (pub) {
       // trigger stuff here
       console.log("topic: "+pub.topic);
       console.log("message: "+pub.message);
-      setColour(0, 0, 255, 0);
+     // var status = 'FAILED';
+      var status = 'BUILDING';
+      //var status = 'SUCCESS';
+      // var status = pub.message.status;
+      switch (status) {
+        case 'building':
+          amber();
+          break;
+        case 'passed':
+          green();
+          break;
+        case 'failed':
+        default:
+          red();
+          break;
+      }
     });
 
 
