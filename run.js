e490e88f847baabbfb4270d7f70894c6fbd64049
Gareth Jones <me@gazj.co.uk>
2018-12-20 13:00:33 +0000

    Connect to bus

--- run.js
+++ run.js
@@ -13,15 +13,32 @@ function setColour(red, green, blue, white) {
   neopixel.write(B15, data);
 }
 
-setColour(255, 0, 0, 0);
+function onInit(){
+  setColour(255, 0, 0, 0);
 
 
-wifi.connect("O2 Wifi", {}, function(err){
-  console.log("connected:", err);
-   setColour(0, 255, 0, 0);
+  wifi.connect("O2 Wifi", {}, function(err){
+    console.log("connected:", err);
+     setColour(0, 255, 0, 0);
 
-  wifi.getIP(function(err, ipinfo){
-    console.log(ipinfo);
+    wifi.getIP(function(err, ipinfo){
+      console.log(ipinfo);
+    });
+
+    var mqtt = require("MQTT").connect({
+      host: "10.204.82.73",
+    });
+    mqtt.subscribe("test/espruino");
+
+    mqtt.on('publish', function (pub) {
+      // trigger stuff here
+      console.log("topic: "+pub.topic);
+      console.log("message: "+pub.message);
+      setColour(0, 0, 255, 0);
+    });
+
+
   });
+}
 
-});
+onInit();
