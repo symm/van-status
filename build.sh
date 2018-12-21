8ee5d5c7d48d538bc9932b22fa73d60570e4e7d7
Gareth Jones <me@gazj.co.uk>
2018-12-21 13:39:51 +0000

    Push building message

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 cd client
-LD_LIBRARY_PATH=. ./mosquitto_pub
+LD_LIBRARY_PATH=. ./mosquitto_pub -h ec2-34-243-3-198.eu-west-1.compute.amazonaws.com -m "{"status":"BUILDING"}" -t test/espruino
+sleep 5
