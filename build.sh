e371f362942e6ed3d4076e1a217803b37e6dd46a
Gareth Jones <me@gazj.co.uk>
2018-12-21 13:44:48 +0000

    Try message

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd client
 echo "doing build"
-LD_LIBRARY_PATH=. ./mosquitto_pub -h ec2-34-243-3-198.eu-west-1.compute.amazonaws.com -m "{"status":"BUILDING"}" -t test/espruino
+LD_LIBRARY_PATH=. ./mosquitto_pub -h ec2-34-243-3-198.eu-west-1.compute.amazonaws.com -m '{"status":"BUILDING"}' -t test/espruino
 sleep 5
 echo "done build"
