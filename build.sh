0fc6bc0bd2a99b88b70af443d2a8c759e25e4328
Gareth Jones <me@gazj.co.uk>
2018-12-21 14:41:42 +0000

    break it

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ echo "doing build"
 LD_LIBRARY_PATH=. ./mosquitto_pub -h ec2-34-243-3-198.eu-west-1.compute.amazonaws.com -m '{"status":"BUILDING"}' -t test/espruino
 sleep 5
 echo "done build"
+exit 1
